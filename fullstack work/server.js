f8f6b1c4f872d0e080cc13080cb9cd0676e7f68e
Jona <jonathan.c.rodriguez@protonmail.com>
2025-08-16 02:04:33 -0600

    completed delete

--- fullstack work/server.js
+++ fullstack work/server.js
@@ -42,4 +42,18 @@ app.get('/info', (req, res) =>{
 
 app.get('/api/persons/:id', (req,res) => {
     const id = req.params.id
+    const entry = persons.find(entry =>{
+      entry.id == id
+    })
+    if (entry){
+       res.json(entry)
+    } else {
+      res.status(404).end()
+    }
 })
+
+app.delete('/api/persons/:id', (req,res) => {
+  const id = NUMBER(req.params.id)
+  persons = persons.filter(entry => entry != id)
+  res.status(204).end()
+}))
